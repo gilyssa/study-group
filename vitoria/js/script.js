6cd2a4e9b2d8f4f1974af12154e90f4e373e001c
Vitoria Musa <vitoriamusatb@gmail.com>
2025-07-11 16:46:54 -0300

    Destaca visualmente o card clicado com classe .card-ativo

--- vitoria/js/script.js
+++ vitoria/js/script.js
@@ -23,19 +23,34 @@ document.addEventListener("DOMContentLoaded", () => {
       container.classList.add("ativo");
     }
 
+    function ativarCard(cardClicado) {
+    todosCards.forEach(card => {
+    const interno = card.querySelector('.card-padrao');
+    if (interno) interno.classList.remove("card-ativo");
+  });
+
+  const clicadoInterno = cardClicado.querySelector('.card-padrao');
+  if (clicadoInterno) clicadoInterno.classList.add("card-ativo");
+}
+
+
     cardGerenciar.addEventListener("click",() => {
         mostrarContainer(containerTrilhas);
+        ativarCard(cardGerenciar);
     });
     
     cardEntregas.addEventListener("click", () => {
         mostrarContainer(containerEntregas);
+        ativarCard(cardEntregas);
     });
 
     cardFeedback.addEventListener("click",() => {
         mostrarContainer(containerFeedback);
+         ativarCard(cardFeedback);
     });
 
     cardRelatorios.addEventListener("click", () =>{
         mostrarContainer(containerRelatorios);
+        ativarCard(cardRelatorios);
     });
 });
