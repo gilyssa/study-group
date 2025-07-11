fea5e75c66212317df5bba275f73e0dc30293521
Vitoria Musa <vitoriamusatb@gmail.com>
2025-07-11 15:51:53 -0300

    Refatora exibição do container para esconder todos antes de mostrar trilhas

--- vitoria/js/script.js
+++ vitoria/js/script.js
@@ -1,9 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DomContentLoaded", () => {
     const cardGerenciar = document.querySelector ("#card-gerenciar");
     const containerTrilhas = document.querySelector("#container-trilhas-estudos");
 
-    cardGerenciar.addEventListener("click", () => {
-        containerTrilhas.classList.remove("oculto");
-        containerTrilhas.classList.add("ativo");
+    const todosContainers = document.querySelectorAll(".container-conteudo");
+
+    function mostrarContainer(container) {
+      todosContainers.forEach(c => {
+        c.classList.remove("ativo");
+        c.classList.add("oculto");
+      });
+
+      container.classList.remove("oculto");
+      container.classList.add("ativo");
+    }
+
+    cardGerenciar.addEventListener("click",() => {
+        mostrarContainer(containerTrilhas);
     });
 });
