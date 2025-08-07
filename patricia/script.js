15f6724645c3e796119ed4dc2253f1f092bcf181
PatriciaOp <patriciapelizon@gmail.com>
2025-08-07 17:18:12 -0300

    add funcao do menu hamburguer

--- patricia/script.js
+++ patricia/script.js
@@ -7,19 +7,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const selecaoButtons = document.querySelectorAll('.front , .back , .dados , .ui-ux')
 
-    
-        selecaoButtons.forEach(function (botao) {
 
-           botao.addEventListener('click', function () {
+    selecaoButtons.forEach(function (botao) {
 
-                selecaoButtons.forEach(function(btn){
-                    btn.classList.remove('ativo')
-                })
-                this.classList.add('ativo')
-                    
-            })          
+        botao.addEventListener('click', function () {
+
+            selecaoButtons.forEach(function (btn) {
+                btn.classList.remove('ativo')
+            })
+            this.classList.add('ativo')
+
         })
-    
+    })
+
 
 
 
@@ -30,16 +30,16 @@ document.addEventListener('DOMContentLoaded', function () {
     if (buttonEnviar) {
         buttonEnviar.addEventListener('click', function () {
 
-            
+
             const inputFile = document.createElement('input')
             inputFile.type = 'file'
-            inputFile.accept = '.zip,.rar,.pdf,.html,.css,.js' 
+            inputFile.accept = '.zip,.rar,.pdf,.html,.css,.js'
 
             inputFile.addEventListener('change', function () {
                 if (inputFile.files.length > 0) {
                     const nomeArquivo = inputFile.files[0].name
 
-                  
+
                     alert(`Arquivo "${nomeArquivo}" enviado com sucesso!`)
 
                     status.innerHTML = '<img class="vector3" src="src/Vector3.png" alt="ícone entregue">Entregue'
@@ -47,15 +47,29 @@ document.addEventListener('DOMContentLoaded', function () {
                     status.classList.add('entregue')
 
                     buttonEnviar.innerHTML = '<img class="vector4" src="src/Vector4.png" alt="ícone entregue">Enviar atividade'
-                   
+
                     buttonEnviar.classList.remove('button-enviar')
                     buttonEnviar.classList.add('button-reenviar')
                 }
 
             })
-            inputFile.click() 
+            inputFile.click()
         })
     }
+    const btn = document.getElementById("hamburguer-btn");
+    const menu = document.querySelector(".opcoes ul");
+
+
+
+    btn.addEventListener("click", (e) => {
+        e.stopPropagation();
+        menu.classList.toggle("active");
+    });
+
+
+    document.addEventListener("click", () => {
+        menu.classList.remove("active");
+    });
 
 })
 
